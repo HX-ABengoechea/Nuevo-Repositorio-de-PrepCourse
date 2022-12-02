ce5b155de3d30765e807e3350827fa1079bee953
AlejoBengo <alejobengo@gmail.com>
2022-12-02 16:53:51 -0300

    all solutions

--- 07 - JavaScript V/test/JSV.test.js
+++ 07 - JavaScript V/test/JSV.test.js
@@ -11,85 +11,101 @@ const {
    Persona,
 } = require('../Ejercicio 02');
 
-describe('crearUsuario()', function () {
-   it('Debe retornar una clase con las propiedades "usuario", "nombre", "email" y "password" definidas', function () {
-      const Usuario = crearUsuario();
-      const user = new Usuario(
-         'jssamuel',
-         'Samuel',
-         'samuel@email.com',
-         'LoveJS'
-      );
-      expect(user.usuario).toBe('jssamuel');
-      expect(user.nombre).toBe('Samuel');
-      expect(user.email).toBe('samuel@email.com');
-      expect(user.password).toBe('LoveJS');
-   });
-   it('Debe retornar una clase con un método saludar que retorne el string correspondiente', function () {
-      const Usuario = crearUsuario();
-      const user = new Usuario(
-         'jssamuel',
-         'Samuel',
-         'samuel@email.com',
-         'LoveJS'
-      );
-      expect(user.saludar()).toBe('Hola, mi nombre es Samuel');
-   });
-});
+describe('JAVASCRIPT V', function () {
+   describe('EJERCICIO 01', function () {
+      describe('crearUsuario()', function () {
+         it('Debe retornar una clase con las propiedades "usuario", "nombre", "email" y "password" definidas', function () {
+            const Usuario = crearUsuario();
+            const user = new Usuario(
+               'jssamuel',
+               'Samuel',
+               'samuel@email.com',
+               'LoveJS'
+            );
+            expect(user.usuario).toBe('jssamuel');
+            expect(user.nombre).toBe('Samuel');
+            expect(user.email).toBe('samuel@email.com');
+            expect(user.password).toBe('LoveJS');
+         });
+         it('Debe retornar una clase con un método saludar que retorne el string correspondiente', function () {
+            const Usuario = crearUsuario();
+            const user = new Usuario(
+               'jssamuel',
+               'Samuel',
+               'samuel@email.com',
+               'LoveJS'
+            );
+            expect(user.saludar()).toBe('Hola, mi nombre es Samuel');
+         });
+      });
 
-describe('agregarMetodoPrototype(Constructor)', function () {
-   it('Debe agregar un método "saludar" al contructor de Usuario', function () {
-      function Test() {
-         this.test = true;
-      }
-      agregarMetodoPrototype(Test);
-      const test = new Test();
-      expect(test.saludar()).toBe('Hello World!');
-   });
-});
+      describe('agregarMetodoPrototype(Constructor)', function () {
+         it('Debe agregar un método "saludar" al contructor de Usuario', function () {
+            function Test() {
+               this.test = true;
+            }
+            agregarMetodoPrototype(Test);
+            const test = new Test();
+            expect(test.saludar()).toBe('Hello World!');
+         });
+      });
 
-describe('agregarStringInvertida(StringPrototype)', function () {
-   it('should add a reverse string method to the String prototype that returns a reversed version of the string', function () {
-      agregarStringInvertida();
-      const str = 'Hello';
-      expect(str.reverse()).toBe('olleH');
-   });
-});
-
-describe('Clase Persona', function () {
-   it('Debe crear una instancia "Persona" con las propiedades "nombre", "apellido", "edad" y "domicilio"', function () {
-      const nuevaPersona = new Persona('Juan', 'Pérez', 22, 'Saavedra 123');
-      expect(nuevaPersona).toBeInstanceOf(Persona);
-      expect(user.nombre).toBe('Juan');
-      expect(user.apellido).toBe('Pérez');
-      expect(user.edad).toBe(22);
-      expect(user.domicilio).toBe('Saavedra 123');
-   });
-   it('"Persona" debe tener un método "detalle" que devuelva la información de la persona en forma de objeto', function () {
-      const nuevaPersona = new Persona('Juan', 'Pérez', 22, 'Saavedra 123');
-      expect(nuevaPersona.detalle()).toStrictEqual({
-         nombre: 'Juan',
-         apellido: 'Pérez',
-         edad: 22,
-         domicilio: 'Saavedra 123',
+      describe('agregarStringInvertida(StringPrototype)', function () {
+         it('Debe agregar un método al objeto global "String", invirtiendo la cadena de texto', function () {
+            agregarStringInvertida();
+            const str = 'Hello';
+            expect(str.reverse()).toBe('olleH');
+         });
       });
    });
-});
 
-describe('crearInstanciaPersona()', function () {
-   it('Debe retornar una nueva instancia de "Persona"', function () {
-      expect(
-         crearInstanciaPersona('Juan', 'Pérez', 22, 'Saavedra 123')
-      ).toBeInstanceOf(Persona);
-   });
-});
+   describe('EJERCICIO 02', function () {
+      describe('Clase Persona', function () {
+         it('Debe crear una instancia "Persona" con las propiedades "nombre", "apellido", "edad" y "domicilio"', function () {
+            const nuevaPersona = new Persona(
+               'Juan',
+               'Pérez',
+               22,
+               'Saavedra 123'
+            );
+            expect(nuevaPersona).toBeInstanceOf(Persona);
+            expect(nuevaPersona.nombre).toBe('Juan');
+            expect(nuevaPersona.apellido).toBe('Pérez');
+            expect(nuevaPersona.edad).toBe(22);
+            expect(nuevaPersona.domicilio).toBe('Saavedra 123');
+         });
+         it('"Persona" debe tener un método "detalle" que devuelva la información de la persona en forma de objeto', function () {
+            const nuevaPersona = new Persona(
+               'Juan',
+               'Pérez',
+               22,
+               'Saavedra 123'
+            );
+            expect(nuevaPersona.detalle()).toStrictEqual({
+               nombre: 'Juan',
+               apellido: 'Pérez',
+               edad: 22,
+               domicilio: 'Saavedra 123',
+            });
+         });
+      });
 
-describe('agregarMetodo(Persona)', function () {
-   it('"Persona" debe tener un método "datos" que retorne la información correspondiente', function () {
-      agregarMetodo();
-      const persona = new Persona('Juan', 'Pérez', 22, 'Saavedra 123');
-      persona.nombre === 'Juan' && persona.edad === 22
-         ? expect(persona.datos()).toBe('Juan, 22 años')
-         : expect(persona.datos()).toBe('undefined, undefined años');
+      describe('crearInstanciaPersona()', function () {
+         it('Debe retornar una nueva instancia de "Persona"', function () {
+            expect(
+               crearInstanciaPersona('Juan', 'Pérez', 22, 'Saavedra 123')
+            ).toBeInstanceOf(Persona);
+         });
+      });
+
+      describe('agregarMetodo(Persona)', function () {
+         it('"Persona" debe tener un método "datos" que retorne la información correspondiente', function () {
+            agregarMetodo();
+            const persona = new Persona('Juan', 'Pérez', 22, 'Saavedra 123');
+            persona.nombre === 'Juan' && persona.edad === 22
+               ? expect(persona.datos()).toBe('Juan, 22 años')
+               : expect(persona.datos()).toBe('undefined, undefined años');
+         });
+      });
    });
 });
--- 08 - JavaScript VI/homework.js
+++ 08 - JavaScript VI/homework.js
@@ -5,11 +5,16 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   nombre = nombre.split('');
+   nombre[0] = nombre[0].toUpperCase();
+   return nombre.join('');
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
+   // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -17,6 +22,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -25,6 +31,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   var sum = 0;
+   for (let i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+   }
+   cb(sum);
 }
 
 function forEach(array, cb) {
@@ -32,6 +43,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach((e) => {
+      cb(e);
+   });
 }
 
 function map(array, cb) {
@@ -39,12 +53,18 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   return array.map((e) => {
+      return cb(e);
+   });
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   return arrayOfStrings.filter((e) => {
+      return e[0] === 'a';
+   });
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
