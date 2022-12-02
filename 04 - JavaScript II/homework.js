338d52cbbc09e81720a61d658574f5ed35a14959
AlejoBengo <alejobengo@gmail.com>
2022-12-02 13:14:10 -0300

    Módulo 04, 01 y 02: completed

--- 04 - JavaScript II/homework.js
+++ 04 - JavaScript II/homework.js
@@ -5,6 +5,7 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   return x > y ? x : y;
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +13,7 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   return edad >= 18 ? 'Allowed' : 'Not allowed';
 }
 
 function conection(status) {
@@ -21,6 +23,7 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   return status === 1 ? 'Online' : status === 2 ? 'Away' : 'Offline';
 }
 
 function saludo(idioma) {
@@ -30,6 +33,16 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   switch (idioma) {
+      case 'aleman':
+         return 'Guten Tag!';
+      case 'mandarin':
+         return 'Ni Hao!';
+      case 'ingles':
+         return 'Hello!';
+      default:
+         return 'Hola!';
+   }
 }
 
 function colors(color) {
@@ -41,72 +54,107 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   switch (color) {
+      case 'blue':
+         return 'This is blue';
+      case 'red':
+         return 'This is red';
+      case 'green':
+         return 'This is green';
+      case 'orange':
+         return 'This is orange';
+      default:
+         return 'Color not found';
+   }
 }
 
 function esDiezOCinco(num) {
    // Retornar "true" si "num" es 10 o 5.
    // De lo contrario, retornar "false".
    // Tu código:
+   return num === 5 || num === 10;
 }
 
 function estaEnRango(num) {
    // Retornar "true" si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar "false".
    // Tu código:
+   return num < 50 && num > 20;
 }
 
 function esEntero(num) {
-   // Retornar "true" si "num" es un entero.
+   // Retornar "true" si "num" es un entero, ya sea positivo, negativo o cero.
    // Ejemplo: 0.8   ---> false
    // Ejemplo: 1     ---> true
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna "false".
    // Tu código:
+   return Math.round(num) === num;
 }
 
 function fizzBuzz(num) {
+   // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // Si "num" es divisible entre 3, retorna "fizz".
    // Si "num" es divisible entre 5, retorna "buzz".
-   // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna el número.
    // Tu código:
+   if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+   if (num % 3 === 0) return 'fizz';
+   if (num % 5 === 0) return 'buzz';
+   return num;
 }
 
 function operadoresLogicos(num1, num2, num3) {
    // La función recibe tres números distintos.
-   // Si num1 es mayor a num2 y a num3, y además es positivo, retornar ---> "Número 1 es mayor y positivo".
+   // Si num1 es mayor a num2 y a num3, y además es positivo, retornar ---> "Numero 1 es mayor y positivo".
    // Si alguno de los tres números es negativo, retornar ---> "Hay negativos".
    // Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
    // Si alguno de los argumentos es cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if (num1 > num2 && num1 > num3 && num1 > 0)
+      return 'Numero 1 es mayor y positivo';
+   if (num1 < 0 || num2 < 0 || num3 < 0) return 'Hay negativos';
+   if (num3 > num1 && num3 > num2) return num3 + 1;
+   if (num1 === 0 || num2 === 0 || num3 === 0) return 'Error';
+   return false;
 }
 
 function esPrimo(num) {
-   // Retornar "true" si "num" es primo.
-   // De lo contrario retorna "falso".
+   // Retornar True si "num" es primo.
+   // De lo contrario retorna False.
    // [Pista 1]: un número primo sólo es divisible por sí mismo y por 1.
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
-   // [Nota]: los números 0 y 1 NO son números primos.
+   // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if (num <= 1) return false;
+   for (var i = 2; i <= num - 1; i++) if (num % i == 0) return false;
+   return true;
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   return valor ? 'Soy verdadero' : 'Soy falso';
 }
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
-   // La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+   // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   var arr = [];
+   for (let i = 0; i <= 10; i++) {
+      arr.push(i * 6);
+   }
+   return arr;
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar True.
    // Caso contrario, retornar False.
    // Tu código:
+   return num.toString().length === 3;
 }
 
 function doWhile(num) {
@@ -114,6 +162,10 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   for (let i = 0; i < 8; i++) {
+      num = num + 5;
+   }
+   return num;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- 04 - JavaScript II/test/JSII.test.js
+++ 04 - JavaScript II/test/JSII.test.js
@@ -41,11 +41,14 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('conection(status)', function () {
-      it('Debe retornar el estado de conexión del usuario', function () {
+      it('Debe retornar "Online" si el status es 1', function () {
          expect(conection(1)).toBe('Online');
+      });
+      it('Debe retornar "Away" si el status es 2', function () {
          expect(conection(2)).toBe('Away');
+      });
+      it('Debe retornar "Offline" si el status es cualquier otro valor', function () {
          expect(conection(3)).toBe('Offline');
-         expect(conection()).toBe('Offline');
       });
    });
 
@@ -79,11 +82,11 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('esDiezOCinco(num)', function () {
-      it('Debe retornar true si num es 10 o 5', function () {
+      it('Debe retornar True si num es 10 o 5', function () {
          expect(esDiezOCinco(10)).toBe(true);
          expect(esDiezOCinco(5)).toBe(true);
       });
-      it('Debe retornar false si num no es 10 o 5', function () {
+      it('Debe retornar False si num no es 10 o 5', function () {
          expect(esDiezOCinco(11)).toBe(false);
          expect(esDiezOCinco(6)).toBe(false);
          expect(esDiezOCinco(0)).toBe(false);
@@ -92,13 +95,13 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('estaEnRango(num)', function () {
-      it('Debe retornar true si num está en el rango', function () {
+      it('Debe retornar True si num está en el rango', function () {
          expect(estaEnRango(35.5)).toBe(true);
          expect(estaEnRango(40)).toBe(true);
          expect(estaEnRango(49)).toBe(true);
          expect(estaEnRango(21)).toBe(true);
       });
-      it('Debe retornar false si num está fuera de rango', function () {
+      it('Debe retornar False si num está fuera de rango', function () {
          expect(estaEnRango(10)).toBe(false);
          expect(estaEnRango(20)).toBe(false);
          expect(estaEnRango(50)).toBe(false);
@@ -107,29 +110,29 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('esEntero(num)', function () {
-      it('Debe retornar true si num es 5', function () {
-         expect(esEntero(5)).toBe(true);
-      });
-      it('Debe retornar false si num no es entero', function () {
+      it('Debe retornar False si num no es entero', function () {
          expect(esEntero(0.5)).toBe(false);
       });
-      it('Debe retornar true si num es un entero negativo', function () {
+      it('Debe retornar True si num es un entero positivo', function () {
+         expect(esEntero(5)).toBe(true);
+      });
+      it('Debe retornar True si num es un entero negativo', function () {
          expect(esEntero(-20)).toBe(true);
       });
-      it('Debe retornar true si el argumento es cero', function () {
+      it('Debe retornar True si el argumento es cero', function () {
          expect(esEntero(0)).toBe(true);
       });
    });
 
    describe('fizzBuzz(num)', function () {
+      it('Debe retornar "fizzbuzz" si num es divisible por 3 y 5', function () {
+         expect(fizzBuzz(15)).toBe('fizzbuzz');
+      });
       it('Debe retornar "fizz" si num es divisible por 3', function () {
          expect(fizzBuzz(9)).toBe('fizz');
       });
       it('Debe retornar "buzz" si num es divisible por 5', function () {
          expect(fizzBuzz(10)).toBe('buzz');
-      });
-      it('Debe retornar "fizzbuzz" si num es divisible por 3 y 5', function () {
-         expect(fizzBuzz(15)).toBe('fizzbuzz');
       });
       it('Debe retornar false si num NO es divisible por 5 ni 3', function () {
          expect(fizzBuzz(4)).toBe(4);
@@ -137,19 +140,19 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('operadoresLogicos(num1, num2, num3)', function () {
-      it("Debe retornar 'Número 1 es mayor y positivo' si num1 es positivo y mayor que num2 y num3", function () {
+      it("Debe retornar 'Numero 1 es mayor y positivo' si num1 es positivo y mayor que num2 y num3", function () {
          expect(operadoresLogicos(50, 2, 15)).toBe(
-            'Número 1 es mayor y positivo'
+            'Numero 1 es mayor y positivo'
          );
       });
       it("Debe retornar 'Hay negativos' si alguno de los argumentos es negativo", function () {
-         expect(operadoresLogicos(80, -1, 2)).toBe('Hay negativos');
+         expect(operadoresLogicos(-1, 50, 2)).toBe('Hay negativos');
       });
-      it('Debe retornar num3 + 1 si num3 es mayor que num1 y num2', function () {
+      it('Debe retornar (num3 + 1) si num3 es mayor que num1 y num2', function () {
          expect(operadoresLogicos(1, 3, 10)).toBe(11);
       });
       it("Debe retornar 'Error' si alguno de los argumentos es igual a cero", function () {
-         expect(operadoresLogicos(1, 0, 10)).toBe('Error');
+         expect(operadoresLogicos(0, 5, 2)).toBe('Error');
       });
       it('Debe retornar false si no se coincide con alguna de las opciones mencionadas', function () {
          expect(operadoresLogicos(10, 30, 6)).toBe(false);
@@ -157,26 +160,26 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('esPrimo(num)', function () {
-      it('Debe retornar true si num es primo', function () {
+      it('Debe retornar True si num es primo', function () {
          expect(esPrimo(7)).toBe(true);
          expect(esPrimo(97)).toBe(true);
          expect(esPrimo(11)).toBe(true);
       });
-      it('Debe retornar false si num NO es primo', function () {
-         expect(esPrimo(9)).toBe(false);
-         expect(esPrimo(4)).toBe(false);
-         expect(esPrimo(10)).toBe(false);
-         expect(esPrimo(100)).toBe(false);
+      it('Debe retornar False si num NO es primo', function () {
+         expect(esPrimo(-9)).toBe(false);
          expect(esPrimo(0)).toBe(false);
          expect(esPrimo(1)).toBe(false);
+         expect(esPrimo(100)).toBe(false);
+         expect(esPrimo(4)).toBe(false);
+         expect(esPrimo(57)).toBe(false);
       });
    });
 
    describe('esVerdadero(valor)', function () {
-      it('Debe retornar "Soy verdadero" si valor es true', function () {
+      it('Debe retornar "Soy verdadero" si valor es True', function () {
          expect(esVerdadero(true)).toBe('Soy verdadero');
       });
-      it('Debe retornar "Soy falso" si valor es false', function () {
+      it('Debe retornar "Soy falso" si valor es False', function () {
          expect(esVerdadero(false)).toBe('Soy falso');
       });
    });
@@ -190,12 +193,12 @@ describe('JAVASCRIPT II', function () {
    });
 
    describe('tieneTresDigitos(num)', function () {
-      it('Debe retornar true si num tiene tres dígitos', function () {
+      it('Debe retornar True si num tiene tres dígitos', function () {
          expect(tieneTresDigitos(100)).toBe(true);
          expect(tieneTresDigitos(820)).toBe(true);
          expect(tieneTresDigitos(900)).toBe(true);
       });
-      it('Debe retornar false si num no tiene tres dígitos', function () {
+      it('Debe retornar False si num no tiene tres dígitos', function () {
          expect(tieneTresDigitos(10)).toBe(false);
          expect(tieneTresDigitos(20000)).toBe(false);
       });
